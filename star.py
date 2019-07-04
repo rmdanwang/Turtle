126327cb3ae62fca90a5d3137e6cac11a1fbd662
Dan Wang <rm.danwang@gmail.com>
2019-07-04 16:51:33 -0500

    Add files via upload

---
+++ star.py
@@ -0,0 +1,10 @@
+# Homework by Ru Meng(Dan) Wang star.py
+import turtle as t
+wn = t.Screen()
+Star = t.Turtle()
+
+for i in range(5):
+    Star.forward(220)
+    Star.right((180-36))
+
+wn.exitonclick()
---
+++ turtles.py
@@ -0,0 +1,24 @@
+# Homework by Ru Meng(Dan) Wang turtles.py
+#need to fix again
+
+import turtle as t
+wn = t.Screen()
+wn.bgcolor("light green")
+Alex = t.Turtle()
+Alex.shape("turtle")
+Alex.penup()
+Alex.color("blue")
+Alex.pensize(2)
+
+
+for i in range(12):
+    Alex.forward(100)
+    Alex.stamp()
+    Alex.backward(30)
+    Alex.pendown()
+    Alex.forward(10)
+    Alex.penup()
+    Alex.backward(80)
+    Alex.right(30)
+
+wn.exitonclick()
